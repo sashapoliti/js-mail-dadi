ed42034b150e5d5c8fd5ee88bfe8bee2b1a2a436
sashapoliti <sas.politi@gmail.com>
2024-03-11 15:46:30 +0100

    first dice script ok

--- js/script.js
+++ js/script.js
@@ -30,30 +30,11 @@ elButton.addEventListener('click', function() {
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
-// const wrapper = document.createElement('div'); //create div
-// wrapper.className = 'd-flex align-content-center flex-wrap'; //add class
+let dice = [1, 2, 3, 4, 5, 6];
+let button = document.querySelector('.btn.btn-secondary');
 
-// let numBox = 100; //max number boxes
-// let tmpHtml = ''; //string html
-
-// for(let i = 1; i <= numBox; i++) /* cycle for 100 boxes */ {
-
-//     if (i % 3 === 0 && i % 5 === 0) /* multiples of 3 and 5 */ {
-//       let content = 'fizzbuzz';
-//       tmpHtml += ` <div class="box fizzbuzz d-flex justify-content-center align-items-center">${content}</div>`;
-//     } else if (i % 3 === 0) /* multiples of 3 */ {
-//         let content = 'fizz';
-//         tmpHtml += ` <div class="box fizz d-flex justify-content-center align-items-center">${content}</div>`;
-//     } else if (i % 5 === 0) /* multiples of 5 */ {
-//         let content = 'buzz';
-//         tmpHtml += ` <div class="box buzz d-flex justify-content-center align-items-center">${content}</div>`;
-//     } else /* none of the cases */ {
-//         let content = i;
-//         tmpHtml += ` <div class="box d-flex justify-content-center align-items-center">${content}</div>`;
-//     }
-    
-// };
-
-// wrapper.innerHTML = tmpHtml; //string html full of boxes in wrapper
-// const container = document.querySelector('.container'); //take container
-// container.append(wrapper); //append wrapper in container
+button.addEventListener('click', function() {
+    let userPosition = getRndInteger(0, 5);
+    let userNumber = dice[userPosition];
+    console.log(userNumber);
+});
